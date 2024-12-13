5f390594fff08a3b901bf56da4e79776e2913c27
N151P34NU D3C384L V1093L <33691956+2442919@users.noreply.github.com>
2024-12-13 12:20:58 +0100

    Add files via upload

---
+++ enter_name.py
@@ -0,0 +1,5 @@
+
+FirstName = input ("Enter your First Name: ")
+MidName = input ("Enter your Middle Name: ")
+LastName = input ("Enter your Last Name: ")
+print ("Your Name is :" , FirstName, MidName, LastName)
